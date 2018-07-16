6ba52f738defc39dba9b99cd9f1d68f45f3d7b30
qixiaobo <qixiaobo.nj@gmail.com>
2018-07-16 11:42:39 +0800

    增加链接

--- elastalert_modules/dingtalk_alert.py
+++ elastalert_modules/dingtalk_alert.py
@@ -10,14 +10,14 @@
 
 """
 import json
+
 import requests
 from elastalert.alerts import Alerter, DateTimeEncoder
-from requests.exceptions import RequestException
 from elastalert.util import EAException
+from requests.exceptions import RequestException
 
 
 class DingTalkAlerter(Alerter):
-    
     required_options = frozenset(['dingtalk_webhook', 'dingtalk_msgtype'])
 
     def __init__(self, rule):
@@ -25,11 +25,34 @@ class DingTalkAlerter(Alerter):
         self.dingtalk_webhook_url = self.rule['dingtalk_webhook']
         self.dingtalk_msgtype = self.rule.get('dingtalk_msgtype', 'text')
         self.dingtalk_isAtAll = self.rule.get('dingtalk_isAtAll', False)
-        self.digtalk_title = self.rule.get('dingtalk_title', '')
+        self.dingtalk_title = self.rule.get('dingtalk_title', '')
+        self.dingtalk_messageUrl = self.rule.get('dingtalk_messageUrl', 'http://log.f6car.com')
 
     def format_body(self, body):
         return body.encode('utf8')
-    
+
+    def get_payload(self):
+
+        if self.dingtalk_msgtype == 'text':
+            return {
+                "msgtype": self.dingtalk_msgtype,
+                "text": {
+                    "content": body + self.dingtalk_messageUrl
+                },
+                "at": {
+                    "isAtAll": self.dingtalk_isAtAll
+                }
+            }
+        elif self.dingtalk_msgtype == 'link':
+            return {
+                "msgtype": self.dingtalk_msgtype,
+                "link": {
+                    "text": body,
+                    "picUrl": "",
+                    "messageUrl": self.dingtalk_messageUrl
+                }
+            }
+
     def alert(self, matches):
         headers = {
             "Content-Type": "application/json",
@@ -42,13 +65,13 @@ class DingTalkAlerter(Alerter):
                 "content": body
             },
             "at": {
-                "isAtAll":False
+                "isAtAll": self.dingtalk_isAtAll
             }
         }
         try:
-            response = requests.post(self.dingtalk_webhook_url, 
-                        data=json.dumps(payload, cls=DateTimeEncoder),
-                        headers=headers)
+            response = requests.post(self.dingtalk_webhook_url,
+                                     data=json.dumps(payload, cls=DateTimeEncoder),
+                                     headers=headers)
             response.raise_for_status()
         except RequestException as e:
             raise EAException("Error request to Dingtalk: {0}".format(str(e)))
